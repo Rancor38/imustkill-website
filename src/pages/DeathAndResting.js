37cd66ff14a2956e6ced7f2e04813b8cb28bd405
Rancor38 <zak.rancor@hotmail.com>
2024-06-12 21:35:02 -0700

    Redirects added

--- src/pages/DeathAndResting.js
+++ src/pages/DeathAndResting.js
@@ -21,9 +21,8 @@ const DeathAndResting = () => {
         </Section>
         <Section>
             <h2>Dead</h2>
-            <p>When a character dies you aren't out of the game. The dead can still take turns in combat, and aid the fate of their companions.</p>
-            <p>Once dead, you can choose to strike a feature from your character's sheet and add that value to a living player character of your choice. These abilities last until the start of the selected character’s next turn.</p>
-            <p>Once dead, you can choose to strike a feature from your characters sheet and guarantee a crit success based on that attribute to a living player character of your choice. These abilities last until the start of the selected character’s next turn.</p>
+            <p>When a character dies you aren't out of the game. The dead can assist their allies in combat from beyond the grave.</p>
+            <p>Once dead, you can choose to strike a feature from your character's sheet at any time and add value to a living player character of your choice. These abilities last until the start of the selected character’s next turn.</p>
             <p>For example, you can choose to strike off your late character’s body score, and replace a living character’s tests with their Body score with a 1 until the start of their next turn.</p>
             <p>You can cross out Body, Agility, Focus, and Fate in this way. Additionally, you can give up each insight token and provide a living character a reroll on any test, before discarding the token.</p>
             <p>You can do these things at any time, even in response to a monster attack or a PC missing an attack, etc. to attempt to change the result.</p>
